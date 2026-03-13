6980f27782d49f4e904d8f6923b6ca5dff23a1b5
Yassir <yassir.hsahraoui@gmail.com>
2026-03-13 11:54:51 +0100

    Implement user registration form and authentication transition component.

--- Website/src/App.jsx
+++ Website/src/App.jsx
@@ -1,17 +1,11 @@
-import Login from "./components/Auth/Login";
-import Register from "./components/Auth/Register";
+import AuthTransition from "./components/Auth/AuthTransition";
 
-
-function App() {
+export default function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-[#D1DFEC] font-sans">
-      <div className="bg-white w-[1200px] h-[650px] shadow-2xl  relative rounded-xl border border-[#D1DFEC] overflow-hidden">
-        <Login />
-        <Register />
-
+      <div className="bg-white w-[1200px] h-[650px] shadow-2xl relative rounded-xl border border-[#D1DFEC] overflow-hidden">
+        <AuthTransition />
       </div>
     </div>
   );
 }
-
-export default App;
